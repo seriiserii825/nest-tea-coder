d697460fc0acf6bfcf250418c82697ee66675b32
serii burduja <seriiburduja@gmail.com>
2025-12-02 17:56:59 +0200

    feat: typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TaskModule } from './task/task.module';
 import { MoviesModule } from './movies/movies.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dataSourceOptions } from './config/data-source';
 
 @Module({
-  imports: [TaskModule, MoviesModule],
+  imports: [TypeOrmModule.forRoot(dataSourceOptions), TaskModule, MoviesModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/data-source.ts
@@ -0,0 +1,22 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { config } from 'dotenv';
+
+config();
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+  username: process.env.DB_USERNAME || 'postgres',
+  password: process.env.DB_PASSWORD || 'secret_pass',
+  database: process.env.DB_NAME || 'nestjs-course',
+  // entities: ['dist/**/*.entity.js', 'src/**/*.entity.js'],
+  entities: ['dist/**/*.entity.js'],
+  // migrations: ['dist/db/migrations/*.js'],
+  // seeds: ['dist/src/db/seeders/*.js'],
+  synchronize: true,
+  logging: true,
+};
+
+const dataSource = new DataSource(dataSourceOptions);
+export default dataSource;
