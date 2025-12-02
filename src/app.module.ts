00a876856a41d36f687bd0060effbe961f6edbe9
serii burduja <seriiburduja@gmail.com>
2025-12-02 16:39:18 +0200

    feat: start movies

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TaskModule } from './task/task.module';
+import { MoviesModule } from './movies/movies.module';
 
 @Module({
-  imports: [TaskModule],
+  imports: [TaskModule, MoviesModule],
   controllers: [AppController],
   providers: [AppService],
 })
