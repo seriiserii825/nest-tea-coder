bca4afcc219e78313cb23ac66573a3a515986a06
serii <seriiburduja@gmail.com>
2025-12-05 21:58:22 +0200

    feat: create all tables

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,15 +1,18 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaClient } from './generated/prisma/client';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService extends PrismaClient implements OnModuleInit {
   constructor() {
-    super({
-      datasources: {
-        db: {
-          url: process.env.DATABASE_URL
-        }
-      }
-    });
+    const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+    const adapter = new PrismaPg(pool);
+    
+    super({ adapter });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
   }
 }
