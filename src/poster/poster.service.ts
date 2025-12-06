a114232c62e2f64af505870a41d00b3c3a261c32
serii <seriiburduja@gmail.com>
2025-12-06 09:10:25 +0200

    feat: poster crud prisma

--- src/poster/poster.service.ts
+++ src/poster/poster.service.ts
@@ -3,36 +3,39 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { MoviePosterEntity } from './entities/poster.entity';
 import { Repository } from 'typeorm';
 import { CreatePosterDto } from './dto/create-poster.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { MoviePoster } from '@prisma/client';
 
 @Injectable()
 export class PosterService {
-  // constructor(
-  //   @InjectRepository(MoviePosterEntity)
-  //   private readonly posterRepository: Repository<MoviePosterEntity>,
-  // ) {}
-  //
-  // async findAll(): Promise<MoviePosterEntity[]> {
-  //   return this.posterRepository.find();
-  // }
-  //
-  // async findById(id: number): Promise<MoviePosterEntity> {
-  //   const poster = await this.posterRepository.findOne({
-  //     where: { id },
-  //   });
-  //   if (!poster) {
-  //     throw new NotFoundException(`Poster with ID ${id} not found`);
-  //   }
-  //   return poster;
-  // }
-  //
-  // async create(dto: CreatePosterDto): Promise<MoviePosterEntity> {
-  //   const poster = this.posterRepository.create(dto);
-  //   try {
-  //     return this.posterRepository.save(poster);
-  //   } catch (error) {
-  //     throw new Error(
-  //       `Failed to create poster: ${error instanceof Error ? error.message : 'Unknown error'}`,
-  //     );
-  //   }
-  // }
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async findAll(): Promise<MoviePoster[]> {
+    return this.prismaService.moviePoster.findMany();
+  }
+
+  async findById(id: number): Promise<MoviePoster> {
+    const poster = await this.prismaService.moviePoster.findUnique({
+      where: { id },
+    });
+    if (!poster) {
+      throw new NotFoundException(`Poster with ID ${id} not found`);
+    }
+    return poster;
+  }
+
+  async create(dto: CreatePosterDto): Promise<MoviePoster> {
+    try {
+      const poster = this.prismaService.moviePoster.create({
+        data: {
+          url: dto.url,
+        },
+      });
+      return poster;
+    } catch (error) {
+      throw new Error(
+        `Failed to create poster: ${error instanceof Error ? error.message : 'Unknown error'}`,
+      );
+    }
+  }
 }
--- src/reviews/reviews.service.ts
+++ src/reviews/reviews.service.ts
@@ -1,18 +1,12 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { ReviewEntity } from './entities/review.entity';
-import { Repository } from 'typeorm';
-import { MoviesService } from 'src/movies/movies.service';
-import { CreateReviewDto } from './dto/create-review.dto';
+import {PrismaService} from 'src/prisma/prisma.service';
 
 @Injectable()
 export class ReviewsService {
-  // constructor(
-  //   @InjectRepository(ReviewEntity)
-  //   private readonly reviewRepository: Repository<ReviewEntity>,
-  //   private readonly movieService: MoviesService,
-  // ) {}
-  //
+  constructor(
+    private readonly prismaService: PrismaService,
+  ) {}
+
   // async create(dto: CreateReviewDto): Promise<ReviewEntity> {
   //   const movie = await this.movieService.findById(dto.movie_id);
   //   const review = this.reviewRepository.create({ ...dto, movie });
